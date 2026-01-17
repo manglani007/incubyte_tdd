36fd23f1c02d32d65f35acf9ee155f759ade8727
Rohit Manglani <52718917+manglani007@users.noreply.github.com>
2026-01-18 02:14:33 +0530

    implement: supports custom delimiter

--- add.dart
+++ add.dart
@@ -1,5 +1,15 @@
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
-  final commaSeparatedNumbers = numbers.replaceAll('\n', ',');
+  String delimiter = ',';
+  String data = numbers;
+
+  if (numbers.startsWith('//')) {
+    final parts = numbers.split('\n');
+    delimiter = parts[0].substring(2);
+    data = parts[1];
+  }
+
+  final commaSeparatedNumbers = data.replaceAll('\n', delimiter).replaceAll(delimiter, ',');
+
   return commaSeparatedNumbers.split(',').map(int.parse).fold(0, (sum, n) => sum + n);
 }
