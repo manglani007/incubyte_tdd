b08c1e72a2aeb2f744a28026a178d815e7e355d7
Rohit Manglani <52718917+manglani007@users.noreply.github.com>
2026-01-18 02:01:26 +0530

    test: sum of comma separated numbers

--- test/add_test.dart
+++ test/add_test.dart
@@ -7,5 +7,10 @@ void main() {
     test('returns 0 for empty string', () {
       expect(add(''), equals(0));
     });
+
+    test('sum of comma separated numbers', () {
+      expect(add('1,2,3,4,5'), equals(15));
+    });
+
   });
 }
